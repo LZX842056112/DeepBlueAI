8c80b77b0d3391d2c7336219ee4f2a96d29e1f39
xing <52873782+LZX842056112@users.noreply.github.com>
2025-09-14 21:11:27 +0800

    第26天,包,文件，路径

---
+++ p261/p26.py
@@ -0,0 +1,10 @@
+print('p26成功')
+
+from p261.p262.p263 import p26_2
+
+print('p263')
+
+print(__name__,'*******')
+
+if __name__ == '__main__':
+    print('************')
---
+++ p261/p262/p263/p26_2.py
@@ -0,0 +1,6 @@
+print('p26_2成功')
+
+from p261.p262.p263 import p26_3
+from p261 import p26
+
+print('p26_2')
