d4e8ea26261e3de63e7fa71824fb34b4a31e86a8
xing <52873782+LZX842056112@users.noreply.github.com>
2025-10-17 11:29:08 +0800

    C++20251016作业

---
+++ 20251016/李宗效20251016.py
@@ -0,0 +1,13 @@
+import time
+
+
+def fibnacci(n):
+    if n <= 1: return n
+    return fibnacci(n - 1) + fibnacci(n - 2)
+
+
+start = time.perf_counter()
+for i in range(1000000):
+    fibnacci(20)
+end = time.perf_counter()
+print((end - start) * 1000000) # 1464381099.0
